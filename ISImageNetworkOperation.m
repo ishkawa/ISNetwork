8770fe3af907f8c5ba0609aaad10def529d66cda
Hiromitsu Murakami <murakami-hiromitsu@kayac.com>
2012-05-01 14:42:17 +0900

    To make it easy to create a subclass

--- ISImageNetworkOperation.m
+++ ISImageNetworkOperation.m
@@ -2,14 +2,6 @@
 
 @implementation ISImageNetworkOperation
 
-+ (id)operationWithRequest:(NSURLRequest *)request
-{
-    ISImageNetworkOperation *operation = [[[ISImageNetworkOperation alloc] init] autorelease];
-    operation.request = request;
-    
-    return operation;
-}
-
 - (id)processData:(NSData *)data
 {
     return [UIImage imageWithData:data];
